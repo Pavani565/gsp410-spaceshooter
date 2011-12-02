ffb1e98da679dd24c40de1dfcc2b5dc269fdc526
cdcredninja@gmail.com <cdcredninja@gmail.com@7eab73fc-b600-5548-9ef9-911d97763ba7>
2011-12-02 23:28:42 +0000

    added those pictures

--- trunk/Code Base/GSP410-Project2/Unit.h
+++ trunk/Code Base/GSP410-Project2/Unit.h
@@ -7,18 +7,23 @@ private:
 	// General Unit Variables			//
 	int m_X;	// X position on Screen //
 	int m_Y;	// Y position on Screen //
-	IDirect3DTexture9* UnitTexture;
+	IDirect3DTexture9*	m_UnitTexture;
+	D3DXIMAGE_INFO		m_TextureInfo;
 	
 public:
 	// General Unit Set Functions //
 	void setX(int newX);
 	void setY(int newY);
 	void setTexturePointer(IDirect3DTexture9* TextureAddress);
+	void setTextureInfo(D3DXIMAGE_INFO TextureInformation);
 
 	// General Unit Get Functions //
 	int getX(void);
 	int getY(void);
-	IDirect3DTexture9* getTexturePointer(void);
+	IDirect3DTexture9*	getTexturePointer(void);
+	D3DXIMAGE_INFO		getTextureInfo(void);
+
+	bool Clicked(int MouseX, int MouseY);
 
 	// Constructor & Destructor //
 	CUnit(void);
